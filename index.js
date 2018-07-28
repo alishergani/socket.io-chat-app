c8ea1d4f06925afcc489c544af6a08398ecf8006
ka4anbek <ka4anbek@gmail.com>
2018-07-28 18:08:42 +0600

    make it better

---
+++ index.js
@@ -0,0 +1,32 @@
+var express = require('express'); //
+var socektIo = require('socket.io');
+var http = require('http');
+
+var app = express();
+var server = http.Server(app);
+var io = socektIo(server)
+
+var port = 3000;
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+	console.log('Connected!#####################')
+  socket.on('message', function(msg){
+    io.emit('message', msg);
+  });
+
+  socket.on('typing', function(data) {
+  	socket.broadcast.emit('typing', data)
+  })
+  socket.on('userconnected', function(name) {
+  	console.log('connected:##:', name)
+  	socket.broadcast.emit('online', name)
+  })
+});
+
+server.listen(port, function(){
+  console.log('listening on *:' + port);
+});
